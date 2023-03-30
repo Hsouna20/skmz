ab0aaaf6bd3c5c1b17a217999845d6e0a7866003
Hassen Mrabet <94061389+Hsouna20@users.noreply.github.com>
2023-03-30 15:35:29 +0200

    Update generated.go

--- server/gql/gen/generated.go
+++ server/gql/gen/generated.go
@@ -12,7 +12,7 @@ import (
 
 	"github.com/99designs/gqlgen/graphql"
 	"github.com/99designs/gqlgen/graphql/introspection"
-	"github.com/shpota/skmz/model"
+	"github.com/Hsouna20/skmz/tree/main/server/model"
 	"github.com/vektah/gqlparser"
 	"github.com/vektah/gqlparser/ast"
 )
